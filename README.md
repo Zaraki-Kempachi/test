b3b9e5f31a06d6ddeef28aec74547483e66900b3
«Zaraki-Kempachi» <coldsnap489@gmail.com>
2021-06-16 17:30:15 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+<<<<<<< HEAD
 # test
+=======
+<<<<<<< HEAD
+# test_2
+=======
+# Zaraki-Kempachi
+>>>>>>> f62b0b7 (first commit)
+>>>>>>> 3aa7ac1 (Перший комміт)
---
+++ praktika.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+long double get_fibonnachi(int);
+
+int clean_stdin() {
+	while (getchar() != '\n');
+	return 1;
+}
+
+int main() 
+{
+	char c;
+	int pnum, k;
+	do {
+		printf("Fibonacci series ordinal number: ");
+	} while
+		(((scanf_s("%d%c", &pnum, &c) != 2 || c != '\n') && clean_stdin()));
+	k = pnum;
+	printf("+-----------+---------------+\n");
+	printf("|   nomer   |    znachenie  |\n");
+	printf("+-----------+---------------+\n");
+	for (pnum; pnum<k+10; pnum++) {
+		if (pnum < 0)
+			pnum = k = 0;
+		printf("| %5d     |    %6lg     |\n", pnum, get_fibonnachi(pnum));
+		printf("+-----------+---------------+\n");
+	}
+}
+
+long double get_fibonnachi(int num) {
+	if (num == 0 || num == 1)
+		return 1;
+	return get_fibonnachi(num - 1) + get_fibonnachi(num - 2); 
+}
